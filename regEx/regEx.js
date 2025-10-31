21dac92ca91668a6afb5cdea5fc0f4417f413418
samuelpondugala <samueldavid.webdev@gmail.com>
2025-10-31 18:28:01 +0530

    Remove React UI Design Project files and add regex validation examples

---
+++ regEx/regEx.js
@@ -0,0 +1,42 @@
+// Check given string contains aleast one lowercase Alphabet or not
+// let s = "rianoianorberign"
+// let regEx = /[a-z]+/
+// console.log(regEx.test(s))
+// s = "19898984988498498498498498"
+// console.log(regEx.test(s))
+// check string contains all the lowercase alphabets or not
+// let s = "sjoaif"
+// let reg = /^[a-z]+$/
+// console.log(reg.test(s))
+// s = "NvoainrovOINOnvoai"
+// console.log(reg.test(s))
+// check given string starts with alphabet or not
+// let s = "nvoiarbvo"
+// let reg = /^[a-z]/
+// console.log(reg.test(s))
+// s = '56tmnoiabn6no4i6o'
+// consle.log(reg.test(s))
+
+// check string contains any non-Alphabetical character is there or not
+// let s = "sdh8nroaub vb"
+// let reg = /[^a-z]/i
+// console.log(reg.test(s))
+// s = "djroipbganopirbNPOINbopiapm"
+// console.log(reg.test(s))
+// check string contains atleast 4 conscutive alphabets are there or not
+// let s = "voainrobianoioinobv"
+// let reg = /[a-z]{4,1}/i
+// console.log(reg.test(s))
+// s = "dfjD7nva"
+// console.log(reg.test(s))
+// check string contains all are digits or not
+// let s = "5385638bvi"
+// let reg = /^\d+$/
+// console.log(reg.test(s))
+// s = "16519815841981"
+// console.log(reg.test(s))
+// extract all 3 lenth substings that was tracking ending with k
+// let s= "abcdefghijk"
+// let reg = /a.k/g
+// console.log(reg.test(s))
+// Extract all numerical values from a given stringe
